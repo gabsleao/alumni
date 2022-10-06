4eb468b17078b807533c8206d1fcbdc63b033785
gabriel leão <gabidonn@gmail.com>
2022-10-05 23:26:19 -0300

    commit inicial

---
+++ public/config/assets_loader.php
@@ -0,0 +1,4 @@
+<head>
+    <link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\bootstrap\js\bootstrap.bundle.min.js">
+</head>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
-
-echo 'oi';
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/components/navbar.php';
