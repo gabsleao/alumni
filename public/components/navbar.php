5667147847a108d061ed1633e6070d3c29328c50
gabriel leão <gabidonn@gmail.com>
2022-10-12 00:43:00 -0300

    nova navbar

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -1,17 +1,23 @@
-<nav class="navbar bg-light shadow-sm mb-5 rounded">
-    <div class="container-fluid">
-        <!-- logo -->
-        <a class="navbar-brand ms-3" href="index.php">
-            <img src="assets/icons/logo.svg" alt="Bootstrap" width="50" height="60">
-        </a>
+<nav class="bg-light shadow-sm mb-5 rounded">
+    <div class="row">
+        <div class="col-11 mt-2 mb-2">
+            <a class="navbar-brand ms-3" href="index.php">
+                <img src="assets/icons/logo.svg" alt="Bootstrap" width="50" height="60">
+            </a>
+        </div>
 
-        <!-- user icon -->
-        <div class="d-flex px-3">
+        <!-- <div class="col-2 mt-2">
+                <div class="d-flex align-items-end flex-column">
+                    <button class="dropdown-item" type="button"><img src="assets/icons/location.svg" width="20" height="20"> Pegar localização atual</button>
+                </div>
+            </div> -->
+
+        <div class="col-1 mt-2 mb-2">
             <div class="dropdown">
-                <a class="navbar-brand ms-3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="ms-3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="assets/icons/guest_caret_down.svg" alt="Usuário" width="50" height="50" id="userIcon">
                 </a>
-                <ul class="dropdown-menu dropdown-menu-end rounded-3 text-muted" id="dropdown">
+                <ul class="dropdown-menu dropdown-menu-end rounded-3 text-muted mt-3" id="dropdown">
                     <p class="dropdown-item">
                         Olá, visitante! Seja bem-vindo(a).
                     </p>
@@ -27,8 +33,6 @@
                 </ul>
             </div>
         </div>
-
-
     </div>
 </nav>
 
@@ -38,7 +42,7 @@
             document.getElementById("userIcon").src = "assets/icons/guest_caret_up.svg";
         });
 
-        $('.dropdown').on('hide.bs.dropdown', function(e) {
+        $(".dropdown").on("hide.bs.dropdown", function(e) {
             document.getElementById("userIcon").src = "assets/icons/guest_caret_down.svg";
         });
     });
