8fbbd2658ae71d39d0b5db2199b6feaba2d5bfed
gabriel leão <gabidonn@gmail.com>
2022-10-13 01:56:50 -0300

    remover log

--- public/controllers/endpoint.php
+++ public/controllers/endpoint.php
@@ -9,7 +9,6 @@ $AbstractController = new AbstractController($_POST["controller"]);
 
 switch ($_POST["operacao"]) {
     case "registrar_usuario":
-        Log::doLog(var_export($AbstractController, 1), 'backtrace', 1);
         $AbstractController->Controller->nome = $_POST["nome"];
         $AbstractController->Controller->email = $_POST["email"];
         $AbstractController->Controller->senha = $_POST["senha"];
@@ -18,7 +17,7 @@ switch ($_POST["operacao"]) {
         $AbstractController->Controller->informacoes["profile_img_filename"] = $_POST["profile_img_filename"];
         $AbstractController->Controller->informacoes["cidade"] = $_POST["cidade"];
         $AbstractController->Controller->informacoes["estado"] = $_POST["estado"];
-        Log::doLog(var_export($AbstractController, 1), 'backtrace');
+        
         $AbstractController->Controller->criar();
         break;
 
