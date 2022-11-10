0cc43cfdff6ea73a2932f6ae4d01d951c5327f06
Gabriel Silva <gabidonn@gmail.com>
2022-11-09 21:38:43 -0300

    ajuste navbar container e row

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -10,48 +10,50 @@ $Logado = isset($_SESSION["Session"]) ? true : false;
 ?>
 
 <nav class="bg-light shadow-sm mb-3 rounded">
-    <div class="row">
-        <div class="col-11 mt-2 mb-2">
-            <a class="navbar-brand ms-3" href="index.php">
-                <img src="assets/icons/logo.svg" alt="Bootstrap" width="50" height="60">
-            </a>
-        </div>
-
-        <!-- <div class="col-2 mt-2">
-                <div class="d-flex align-items-end flex-column">
-                    <button class="dropdown-item" type="button"><img src="assets/icons/location.svg" width="20" height="20"> Pegar localização atual</button>
-                </div>
-            </div> -->
-
-        <div class="col-1 mt-2 mb-2">
-            <div class="dropdown">
-                <a class="ms-3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="<?= $UserIcon ?>" alt="Usuário" width="50" height="50" id="userIcon">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-11 mt-2 mb-2">
+                <a class="navbar-brand ms-3" href="index.php">
+                    <img src="assets/icons/logo.svg" alt="Bootstrap" width="50" height="60">
                 </a>
-                <ul class="dropdown-menu dropdown-menu-end rounded-3 text-muted mt-3" id="dropdown">
-                    <p class="dropdown-item">
-                        Olá, <u><b><?= $Username ?></b></u>! Seja bem-vindo(a).
-                    </p>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <?php if (!$Logado) { ?>
-                        <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalLogin"><img src="assets/icons/sign_in.svg" width="20" height="20"> Entrar com uma conta</button></li>
-                        <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalRegistrar"><img src="assets/icons/sign_up.svg" width="20" height="20"> Registrar</button></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalRecuperarSenha"><img src="assets/icons/forgot_pass.svg" width="20" height="20"> Esqueceu sua senha?</button></li>
-                    <?php } else { ?>
-                        <li><button class="dropdown-item" type="button" onclick="window.location.href = './public/perfil_usuario.php';"><img src="assets/icons/user_list.svg" width="20" height="20"> Ver perfil</button></li>
-                        <li><button class="dropdown-item" type="button"><img src="assets/icons/gear.svg" width="20" height="20"> Configurações</button></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalDeslogar"><img src="assets/icons/power.svg" width="20" height="20"> Sair</button></li>
-                    <?php } ?>
+            </div>
 
-                </ul>
+            <!-- <div class="col-2 mt-2">
+                    <div class="d-flex align-items-end flex-column">
+                        <button class="dropdown-item" type="button"><img src="assets/icons/location.svg" width="20" height="20"> Pegar localização atual</button>
+                    </div>
+                </div> -->
+
+            <div class="col-1 mt-2 mb-2">
+                <div class="dropdown">
+                    <a class="ms-3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="<?= $UserIcon ?>" alt="Usuário" width="50" height="50" id="userIcon">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end rounded-3 text-muted mt-3" id="dropdown">
+                        <p class="dropdown-item">
+                            Olá, <u><b><?= $Username ?></b></u>! Seja bem-vindo(a).
+                        </p>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <?php if (!$Logado) { ?>
+                            <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalLogin"><img src="assets/icons/sign_in.svg" width="20" height="20"> Entrar com uma conta</button></li>
+                            <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalRegistrar"><img src="assets/icons/sign_up.svg" width="20" height="20"> Registrar</button></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalRecuperarSenha"><img src="assets/icons/forgot_pass.svg" width="20" height="20"> Esqueceu sua senha?</button></li>
+                        <?php } else { ?>
+                            <li><button class="dropdown-item" type="button" onclick="window.location.href = './public/perfil_usuario.php';"><img src="assets/icons/user_list.svg" width="20" height="20"> Ver perfil</button></li>
+                            <li><button class="dropdown-item" type="button"><img src="assets/icons/gear.svg" width="20" height="20"> Configurações</button></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><button class="dropdown-item" type="button" data-bs-toggle="modal" data-bs-target="#modalDeslogar"><img src="assets/icons/power.svg" width="20" height="20"> Sair</button></li>
+                        <?php } ?>
+
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
