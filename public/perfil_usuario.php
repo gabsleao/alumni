66c2b5592601c97330e5775f8ce8e52f5b97b3cc
Gabriel Silva <gabidonn@gmail.com>
2022-11-09 21:41:21 -0300

    tela perfil usuário

--- public/perfil_usuario.php
+++ public/perfil_usuario.php
@@ -27,113 +27,53 @@ require_once __DIR__ . '/components/navbar.php';
           <div class="card-body p-0">
             <ul class="list-group list-group-flush rounded-3">
               <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i class="fas fa-globe fa-lg text-warning">Nome</i>a
-                <p class="mb-0">-</p>
+                <p class="mb-0">Nome</p><?= $_SESSION["Session"]->Usuario->nome; ?>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i class="fab fa-github fa-lg" style="color: #333333;">Email</i>b
-                <p class="mb-0">-</p>
+                <p class="mb-0">Email</p><?= Seguranca::decryptString($_SESSION["Session"]->Usuario->email); ?>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i class="fab fa-twitter fa-lg" style="color: #55acee;">Tipo</i>c
-                <p class="mb-0">-</p>
+                <p class="mb-0">Tipo</p><?= $_SESSION["Session"]->Usuario->tipo; ?>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i class="fab fa-instagram fa-lg" style="color: #ac2bac;">Cidade</i>d
-                <p class="mb-0">-</p>
+                <p class="mb-0">Cidade</p><?= $_SESSION["Session"]->Usuario->informacoes["cidade"] ?>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;">Estado</i>e
-                <p class="mb-0">-</p>
+                <p class="mb-0">Estado</p><?= $_SESSION["Session"]->Usuario->informacoes["estado"] ?>
               </li>
             </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-8">
-        <div class="card mb-4">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Nome</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0"><?= $_SESSION["Session"]->Usuario->nome; ?></p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Email</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0"><?= Seguranca::decryptString($_SESSION["Session"]->Usuario->email); ?></p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Tipo</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0"><?= $_SESSION["Session"]->Usuario->tipo; ?></p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Cidade</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0"><?= $_SESSION["Session"]->Usuario->informacoes["cidade"]; ?></p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Estado</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0"><?= $_SESSION["Session"]->Usuario->informacoes["estado"]; ?></p>
-              </div>
-            </div>
-          </div>
-        </div>
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-md-12 mb-4">
             <div class="card mb-4 mb-md-0">
               <div class="card-body">
-                <p class="mb-4">Acessado recentemente
-                </p>
-                <p class="mb-1" style="font-size: .77rem;">Nome da Instituição</p>
-                <div class="progress rounded" style="height: 5px;">
-                  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Nome da Instituição</p>
-                <div class="progress rounded" style="height: 5px;">
-                  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Nome da Instituição</p>
-                <div class="progress rounded" style="height: 5px;">
-                  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Nome da Instituição</p>
-                <div class="progress rounded" style="height: 5px;">
-                  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <p class="mt-4 mb-1" style="font-size: .77rem;">Nome da Instituição</p>
-                <div class="progress rounded mb-2" style="height: 5px;">
-                  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100"
-                    aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                <p class="mb-4">Acessado recentemente</p>
+                <li class="d-flex justify-content-between align-items-center">
+                  <p class="mb-1">Nome da Instituição</p>Acessado em:<?= "data"; ?>
+                </li>
+
+                <li class="d-flex justify-content-between align-items-center">
+                  <p class="mb-1">Nome da Instituição</p>Acessado em:<?= "data"; ?>
+                </li>
+
+                <li class="d-flex justify-content-between align-items-center">
+                  <p class="mb-1">Nome da Instituição</p>Acessado em:<?= "data"; ?>
+                </li>
+
+                <li class="d-flex justify-content-between align-items-center">
+                  <p class="mb-1">Nome da Instituição</p>Acessado em:<?= "data"; ?>
+                </li>
+
+                <li class="d-flex justify-content-between align-items-center">
+                  <p class="mb-1">Nome da Instituição</p>Acessado em:<?= "data"; ?>
+                </li>                
               </div>
             </div>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-12">
             <div class="card mb-4 mb-md-0">
               <div class="card-body">
                 <p class="mb-4">Interesses
