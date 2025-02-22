af1d1c6caecc7ab0b910657c1ceed991c06cae91
gabsleao <gabidonn@gmail.com>
2025-02-22 14:50:05 -0300

    tratamento pra instituicao ja existente

--- public/components/toasts.php
+++ public/components/toasts.php
@@ -163,4 +163,19 @@
             Algo deu errado, por favor tente novamente!
         </div>
     </div>
+
+    <div class="toast fade" id="toastInstituicaoJaExiste">
+        <div class="toast-header">
+            <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <rect width="100%" height="100%" fill="#ff0000"></rect>
+            </svg>
+
+            <strong class="me-auto">Whoops!</strong>
+            <small>agora mesmo</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Fechar"></button>
+        </div>
+        <div class="toast-body">
+            O nome da instituição digitado já existe na nossa plataforma!
+        </div>
+    </div>
 </div>
