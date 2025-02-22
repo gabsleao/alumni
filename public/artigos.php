e68848bd0512d4554b786aff92712d106d45462d
gabsleao <gabidonn@gmail.com>
2025-02-22 14:49:00 -0300

    separar artigos

---
+++ public/artigos.php
@@ -0,0 +1,31 @@
+<div class="col-2">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-light rounded">
+        <button class="btn p-3 border-bottom" type="button" data-bs-toggle="modal" data-bs-target="#modalBoasPraticas">
+            <img src="assets/icons/artigo.svg" width="20" height="20"><span class="fs-5 fw-semibold ms-2">Artigos de
+                Boas Práticas</span>
+        </button>
+        <div class="list-group list-group-flush border-bottom" style="height: 780px; overflow-y: scroll;">
+            <a href="#" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                <div class="d-flex w-100 align-items-center justify-content-between">
+                    <strong class="mb-1">Instituições Duplicadas</strong><small>15/11/2022</small>
+                </div>
+                <div class="col-10 mb-1 small">Antes de adicionar uma instituição de ensino, procure verificar se a
+                    mesma já não foi adicionada.</div>
+                <p class="col-10 mb-1 small">
+                    última edição: <i>15/11/2022</i>
+                </p>
+            </a>
+
+            <a href="#" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                <div class="d-flex w-100 align-items-center justify-content-between">
+                    <strong class="mb-1">Fake News!</strong><small>15/11/2022</small>
+                </div>
+                <div class="col-10 mb-1 small">Cuidado ao adicionar as informações das instituições e/ou cursos.
+                    Informações falsas não serão toleradas.</div>
+                <p class="col-10 mb-1 small">
+                    última edição: <i>15/11/2022</i>
+                </p>
+            </a>
+        </div>
+    </div>
+</div>
