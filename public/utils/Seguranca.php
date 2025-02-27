ad64522f765bb9b8e6016657c741f152d152c82f
gabsleao <gabidonn@gmail.com>
2025-02-27 11:56:16 -0300

    remover comentario

--- public/utils/Seguranca.php
+++ public/utils/Seguranca.php
@@ -1,7 +1,6 @@
 <?php
 
 class Seguranca {
-    private static String $KEY = "ENCRYPT-alumni1923787";
 
     public static function encryptString(String $String): String {
         $EncryptedString = base64_encode(base64_encode(base64_encode($String)));
